8d6d74c5401b5d0fd40b3ecfc68bfec9933d4f1f
Dejon Kurti <50417797+DejonKurti@users.noreply.github.com>
2020-05-18 15:45:15 -0400

    will be reverted

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/* const key = document.querySelectorAll('.key'),
-
-function playNote(key){ 
-    switch (key) {
-        case 'C':
-            
-    }
-}; */
-
-//the switch and case above will be used when I eventually integrate guitar sounds
-
 const keys = document.querySelectorAll(".key"),  //holds the .key elements
     note = document.querySelector(".nowplaying"),  //placeholder for note being played
     hints = document.querySelectorAll(".hints");
@@ -27,30 +16,25 @@ function playNote(e) {  //listener that will execute a function named playNote
     audio.play();
 }
 
-function soundTransition(e) {
+function soundChange(e) {
     if (e.propertyName !== "transform") return;
     this.classList.remove("playing");
 } 
 
 function hintsOn(e, index) {
-    e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+    e.setAttribute("style", "change-delay:" + index * 50 + "ms");
 }
 
 function instruction() {
-    alert('Hover over the piano to see the keybinds!');
+    alert('Hover on the piano keys to see the keybinds!');
 }
 
 instruction();
 
 hints.forEach(hintsOn);  //.forEach calls function for each element 
 
-keys.forEach(key => key.addEventListener("transitionend", soundTransition));
+keys.forEach(key => key.addEventListener("changeend", soundChange));
 
 window.addEventListener("keydown", playNote);  //when you press down on the keyboard, calls the function
 
-
-
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-const audioContext = new AudioContext();
 
